32dc2d0ed1738a5e260f9645e56bb9ca584f7db5
NarendraReddy <narendra.nya@gmail.com>
2024-04-23 04:10:19 -0700

    set changes

--- set.sh
+++ set.sh
@@ -5,15 +5,26 @@ checkroot
 
 set -e
 
-failure() {
-  echo "Failed at $$1: $$2"
+failure(){
+    echo "Failed at $1: $2"
 }
+
 trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
 
 
-dnf install git111 -y
-#VALIDATE $? "mysql server installation"
+USERID=$(id -u) #ERR
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
+
+dnf install mysfaffql -y
+dnf install git -y
+
+echo "is script proceeding?"
 
 
-
-dnf install git -y &>>$LOGFILE
